d9295328ccf069e6ce802b9d1346b3029e5afb7d
Chrisb616 <cboynton16@gmail.com>
2016-11-19 09:34:46 -0500

    Initial Setup

---
+++ message-in-a-bottle/Pods/Target Support Files/Pods-message-in-a-bottle/Pods-message-in-a-bottle-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_message_in_a_bottle : NSObject
+@end
+@implementation PodsDummy_Pods_message_in_a_bottle
+@end
---
+++ message-in-a-bottle/Pods/Target Support Files/Pods-message-in-a-bottle/Pods-message-in-a-bottle-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_message_in_a_bottleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_message_in_a_bottleVersionString[];
+
