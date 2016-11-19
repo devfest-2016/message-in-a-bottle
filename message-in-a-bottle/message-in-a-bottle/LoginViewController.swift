9785dbf7fa045a70af79705ae9fb6702eb3983e4
Ben Su <bensu1013@hotmail.com>
2016-11-19 10:27:37 -0500

    login added

---
+++ message-in-a-bottle/message-in-a-bottle/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.swift
+//  message-in-a-bottle
+//
+//  Created by Benjamin Su on 11/19/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func loginButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func signupButtonAction(_ sender: UIButton) {
+    }
+    
+    
+    
+
+}
