59ca77ef0bf6deb01664fad3616154d32b94203a
daveneff <beefylogs@gmail.com>
2016-11-19 16:51:45 -0500

    Completed OceansCollectionVC icon design

--- message-in-a-bottle/message-in-a-bottle/ChatroomViewController.swift
+++ message-in-a-bottle/message-in-a-bottle/ChatroomViewController.swift
@@ -10,41 +10,11 @@ import UIKit
 
 class ChatroomViewController: UIViewController {
     
-    
-    
     let tableView = UITableView()
     let textField = UITextField()
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //TableView
-                self.view.addSubview(tableView)
-                tableView.translatesAutoresizingMaskIntoConstraints = false
-                tableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
-                tableView.widthAnchor.constraint(equalToConstant: view.widthAnchor).isActive = true
-                tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-                tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        
-        
-        //        //Textfield
-                self.view.addSubview(textField)
-                textField.translatesAutoresizingMaskIntoConstraints = false
-                textField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
-                textField.widthAnchor.constraint(equalToConstant: view.widthAnchor).isActive = true
-                textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-                textField.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
-        //        //Send message button
-                self.view.addSubview(tableView)
-                tableView.translatesAutoresizingMaskIntoConstraints = false
-                tableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
-                tableView.widthAnchor.constraint(equalToConstant: view.widthAnchor).isActive = true
-                tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-                tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        
     }
 
     override func didReceiveMemoryWarning() {
