0ea0986e97b961a61fb1b53693639992a89d39e5
daveneff <beefylogs@gmail.com>
2016-11-19 15:12:17 -0500

    Added function to convert String to Icon

--- message-in-a-bottle/message-in-a-bottle/Icons.swift
+++ message-in-a-bottle/message-in-a-bottle/Icons.swift
@@ -19,13 +19,41 @@ struct IconFont {
 }
 
 struct Icons {
-        static let technology = ""
-        static let music = ""
-        static let media = ""
-        static let fashion = ""
-        static let graphicDesign = ""
-        static let publishing = ""
-        static let finance = ""
-        static let govt = ""
-        static let education = ""
+    static let hospitality = ""
+    static let technology = ""
+    static let music = ""
+    static let media = ""
+    static let fashion = ""
+    static let graphicDesign = ""
+    static let publishing = ""
+    static let finance = ""
+    static let govt = ""
+    static let education = ""
+    
+    static func returnIcon(for string: String) -> String {
+        switch string {
+        case "Hospitality":
+            return hospitality
+        case "Technology":
+            return technology
+        case "Music":
+            return music
+        case "Media":
+            return media
+        case "Fashion":
+            return fashion
+        case "Graphic Design":
+            return graphicDesign
+        case "Publishing":
+            return publishing
+        case "Finance":
+            return finance
+        case "Government":
+            return govt
+        case "Education":
+            return education
+        default:
+            return "error"
+        }
+    }
 }
