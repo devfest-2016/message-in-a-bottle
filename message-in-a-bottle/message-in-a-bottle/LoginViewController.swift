8aba223c46109f7419033c6c159852cc685ae098
Ben Su <bensu1013@hotmail.com>
2016-11-19 16:51:50 -0500

    adding a signup view

--- message-in-a-bottle/message-in-a-bottle/LoginViewController.swift
+++ message-in-a-bottle/message-in-a-bottle/LoginViewController.swift
@@ -14,12 +14,6 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var passwordTextField: UITextField!
     
-    @IBOutlet weak var firstNameTextField: UITextField!
-    
-    @IBOutlet weak var lastNameTextField: UITextField!
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -54,19 +48,115 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func signupButtonAction(_ sender: UIButton) {
-        guard let email = emailTextField.text else {return}
-        guard let password = passwordTextField.text else {return}
-        guard let firstName = firstNameTextField.text else {return}
-        guard let lastName = lastNameTextField.text else {return}
-        FirebaseMethods.signUpButton(email: email, password: password, firstName: firstName, lastName: lastName)
+        
+        let signupview = SignupView(frame: CGRect(x: self.view.frame.size.width * 0.125, y: self.view.frame.size.height, width: self.view.frame.size.width * 0.75, height: self.view.frame.size.height * 0.6))
+        signupview.loadViews()
+        self.view.addSubview(signupview)
+        animateSignupEntry(view: signupview)
+    }
+    
+    func animateSignupEntry(view: UIView) {
+
+        UIView.animate(withDuration: 0.25, animations: {
+            view.center.y = self.view.center.y
+        }) { (success) in
+            print(success)
+        }
+    }
+
+}
+
+
+class SignupView: UIView {
+    
+    var emailTextField: UITextField!
+    var passwordTextField: UITextField!
+    var firstnameTextField: UITextField!
+    var lastnameTextField: UITextField!
+    
+    var signupButton: UIButton!
+    var cancelButton: UIButton!
+    
+    
+    func loadViews() {
+        self.backgroundColor = UIColor.brown
+
+        emailTextField = UITextField(frame: CGRect(x: 0.0, y: frame.size.height * 0.1, width: frame.size.width, height: frame.size.height * 0.08))
+        emailTextField.backgroundColor = UIColor.cyan
+        self.addSubview(emailTextField)
+        passwordTextField = UITextField(frame: CGRect(x: 0.0, y: frame.size.height * 0.2, width: frame.size.width, height: frame.size.height * 0.08))
+        passwordTextField.backgroundColor = UIColor.cyan
+        self.addSubview(passwordTextField)
+        firstnameTextField = UITextField(frame: CGRect(x: 0.0, y: frame.size.height * 0.3, width: frame.size.width, height: frame.size.height * 0.08))
+        firstnameTextField.backgroundColor = UIColor.cyan
+        self.addSubview(firstnameTextField)
+        lastnameTextField = UITextField(frame: CGRect(x: 0.0, y: frame.size.height * 0.4, width: frame.size.width, height: frame.size.height * 0.08))
+        lastnameTextField.backgroundColor = UIColor.cyan
+        self.addSubview(lastnameTextField)
+        
+        signupButton = UIButton(frame: CGRect(x: 0.0, y: frame.size.height * 0.6, width: frame.size.width, height: frame.size.height * 0.08))
+        self.addSubview(signupButton)
+        signupButton.backgroundColor = UIColor.cyan
+        signupButton.titleLabel?.text = "Signup"
+        signupButton.addTarget(self, action: #selector(signupButtonAction), for: .touchUpInside)
+        
+        cancelButton = UIButton(frame: CGRect(x: 0.0, y: frame.size.height * 0.7, width: frame.size.width, height: frame.size.height * 0.08))
+        self.addSubview(cancelButton)
+        cancelButton.backgroundColor = UIColor.red
+        cancelButton.titleLabel?.text = "Cancel"
+        cancelButton.addTarget(self, action: #selector(cancelButtonAction), for: .touchUpInside)
         
     }
     
     
-
+    func signupButtonAction(_ sender: UIButton) {
+        
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        guard let firstName = firstnameTextField.text else {return}
+        guard let lastName = lastnameTextField.text else {return}
+        FirebaseMethods.signUpButton(email: email, password: password, firstName: firstName, lastName: lastName)
+        
+    }
+    
+    func cancelButtonAction(_ sender: UIButton) {
+        
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        guard let firstName = firstnameTextField.text else {return}
+        guard let lastName = lastnameTextField.text else {return}
+        FirebaseMethods.signUpButton(email: email, password: password, firstName: firstName, lastName: lastName)
+        
+    }
+    
+    func animateSignupEntry(view: UIView) {
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.center.y = view.center.y
+        }) { (success) in
+            print(success)
+        }
+    }
+    
+    func animateSignupExit(view: UIView) {
+        
+        UIView.animate(withDuration: 0.25, animations: {
+            self.center.y = self.center.y + view.frame.size.height
+        }) { (success) in
+            print(success)
+        }
+    }
+    
 }
 
-class 
+
+
+
+
+
+
+
+
 
 
 
