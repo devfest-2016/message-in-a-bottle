9459fbc432d005eda0deb513f5e3a6cb18f46302
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-11-19 16:59:15 -0500

    updated chatroom view

--- message-in-a-bottle/message-in-a-bottle/ChatroomViewController.swift
+++ message-in-a-bottle/message-in-a-bottle/ChatroomViewController.swift
@@ -14,36 +14,43 @@ class ChatroomViewController: UIViewController {
     
     let tableView = UITableView()
     let textField = UITextField()
+    let sendButton = UIButton()
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //TableView
+            //TableView
+        
                 self.view.addSubview(tableView)
                 tableView.translatesAutoresizingMaskIntoConstraints = false
-                tableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
-                tableView.widthAnchor.constraint(equalToConstant: view.widthAnchor).isActive = true
+                tableView.bottomAnchor.constraint(equalTo: textField.topAnchor).isActive = true
+                tableView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1.0).isActive = true
                 tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
                 tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         
         
-        //        //Textfield
+            //Textfield
+        
                 self.view.addSubview(textField)
                 textField.translatesAutoresizingMaskIntoConstraints = false
-                textField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
-                textField.widthAnchor.constraint(equalToConstant: view.widthAnchor).isActive = true
-                textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+                textField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15).isActive = true
+                textField.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+                textField.rightAnchor.constraint(equalTo: sendButton.leftAnchor).isActive = true
                 textField.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-        //        //Send message button
-                self.view.addSubview(tableView)
-                tableView.translatesAutoresizingMaskIntoConstraints = false
-                tableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.50).isActive = true
-                tableView.widthAnchor.constraint(equalToConstant: view.widthAnchor).isActive = true
-                tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-                tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            //Send message button
+        
+                self.view.addSubview(sendButton)
+                sendButton.layer.borderWidth = 2.0
+                sendButton.layer.borderColor = UIColor.black.cgColor
+                sendButton.titleLabel?.text = "Send"
+                sendButton.translatesAutoresizingMaskIntoConstraints = false
+                sendButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15).isActive = true
+                sendButton.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+                sendButton.leftAnchor.constraint(equalTo: textField.rightAnchor).isActive = true
+                sendButton.topAnchor.constraint(equalTo: tableView.bottomAnchor).isActive = true
         
     }
 
