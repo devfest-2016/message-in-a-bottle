ada45eab46deb037b1e66f8b048dbcf9ba6b7c44
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-11-19 21:11:03 -0500

    fixed conflicts

--- message-in-a-bottle/message-in-a-bottle/FirebaseMethods.swift
+++ message-in-a-bottle/message-in-a-bottle/FirebaseMethods.swift
@@ -100,7 +100,6 @@ class FirebaseMethods {
         
         print(bottleRef)
         bottleRef.observeSingleEvent(of: .value, with: { (snapshot) in
-            guard let messageInfo = snapshot.value as? [String: Any] else { print("FAILURE: snapshot.value has no value"); return }
 
             guard let messageInfo = snapshot.value as? [String: Any] else { print("FAILURE: snapshot.value has no value for ocean value \(oceanID)"); return }
         
@@ -114,12 +113,6 @@ class FirebaseMethods {
                 let body = messageInfo["body"] as? String,
                 let timestamp = messageInfo["timestamp"] as? Double
                 else { print("FAILURE: Data unavailable in messageInfo");return }
-            
-
-                let body = messageInfo["messageContent"] as? String,
-                let timestampString = messageInfo["timestamp"] as? String,
-                let timestamp = Double(timestampString)
-            else { print("FAILURE: Data unavailable in messageInfo");return }
                 
 
             
