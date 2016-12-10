2b33dd98884e5955b6aea1b0a193924ec375d357
Chrisb616 <cboynton16@gmail.com>
2016-12-10 08:45:38 -0500

    Project Cleanup

---
+++ Pods/Target Support Files/Pods-message-in-a-bottle/Pods-message-in-a-bottle-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_message_in_a_bottleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_message_in_a_bottleVersionString[];
+
--- message-in-a-bottle/Pods/Target Support Files/Pods-message-in-a-bottle/Pods-message-in-a-bottle-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_message_in_a_bottleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_message_in_a_bottleVersionString[];
-
